8099c1a3509fde7e3e37baa0cce8dba388e9cbad
Antarex <63517945+pixaxeofpixie@users.noreply.github.com>
2020-07-18 20:18:57 +0300

    Delete ultimate-cultivator.js

--- scripts/Blocks/ultimate-cultivator.js
+++
@@ -1,7 +0,0 @@
-speed = 10;
-const plantTaron = extendContent(GenericSmelter, "ultimate-cultivator", {
-drawLayer(tile){
-Draw.rect(this.name + "-rotator", tile.drawx(), tile.drawy(), tile.entity.totalProgress*speed + 10);
-}
-});
-plantTaron.layer = Layer.turret
