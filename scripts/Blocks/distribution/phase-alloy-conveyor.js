8d393148e99aa665d0e5e69dc9471c2328f60341
Antarex <63517945+pixaxeofpixie@users.noreply.github.com>
2020-08-11 17:12:30 +0300

    Delete phase-alloy-conveyor.js

--- scripts/Blocks/distribution/phase-alloy-conveyor.js
+++
@@ -1,7 +0,0 @@
-phaseAlloyConveyor = new ItemBridge("phase-alloy-conveyor"){{
-requirements(Category.distribution, ItemStack.with(Items.braindustry-phase-alloy, 5, Items.silicon, 7, Items.titanium, 10, Items.braindustry-graphenite, 10));
-range = 12;
-canOverdrive = true;
-hasPower = true;
-consumes.power(0.30f);
-}};
