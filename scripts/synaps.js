e18d25da2a5128ae09874cf5429d72b692f6f94c
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-12 22:41:22 +0300

    Update synaps.js

--- scripts/synaps.js
+++ scripts/synaps.js
@@ -32,10 +32,10 @@ synaps.size = 3;
 synaps.hasItems = true;
 synaps.hasLiquids = true;
 synaps.localizedName = "Synaps";
-synaps.description = "Sap turret";
+synaps.description = "Electrical Sap turret, can shoot a sap bullet with mane ligthnings.";
 synaps.buildVisibility = BuildVisibility.shown;
 synaps.requirements = ItemStack.with(Items.titanium, 200, Items.lead, 250, Items.silicon, 350, Vars.content.getByName(ContentType.item,"braindustry-odinum"), 150);
-synaps.consumes.items(new ItemStack(Vars.content.getByName(ContentType.item,"braindustry-exotic-alloy"), 150));
+//synaps.consumes.items(new ItemStack(Vars.content.getByName(ContentType.item,"braindustry-exotic-alloy"), 150));
 synaps.category = Category.turret;
 synaps.shootType = electroSapBullet
 synaps.ammo(
