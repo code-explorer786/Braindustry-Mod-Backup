543a1e16fe24e79a7e62d3d6cf18bcefdde9f8a7
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-12 22:51:21 +0300

    Update synaps.js

--- scripts/synaps.js
+++ scripts/synaps.js
@@ -14,14 +14,14 @@ electroSapBullet.damage = 37;
 electroSapBullet.shootEffect = Fx.railShoot;
 electroSapBullet.hitColor = electroSapBullet.color = Color.valueOf("fbff9e");
 electroSapBullet.despawnEffect = Fx.railHit;
-electroSapBullet.width = 4;
+electroSapBullet.width = 3;
 electroSapBullet.lifetime = 45;
 electroSapBullet.knockback = -1;
 electroSapBullet.lightning = 4;//количество молний
 electroSapBullet.lightningLength = 2;//длина молнии
 electroSapBullet.lightningLengthRand = 10;//рандомное число от 0 до 15 будет прибавляться к длине молнии, то есть рандомизация длины
 electroSapBullet.lightningDamage = 50;//урон молнии
-electroSapBullet.lightningAngle = 0;//угол направления молний относительно угла пули
+electroSapBullet.lightningAngle = 6;//угол направления молний относительно угла пули
 electroSapBullet.lightningCone = 12;//рандомизация угла направления молний 
 electroSapBullet.largeHit = true;
 electroSapBullet.lightColor = electroSapBullet.lightningColor = Color.valueOf("fbff9e");
