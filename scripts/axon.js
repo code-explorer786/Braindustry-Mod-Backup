5e1c83925e3d85fbcbe41471921611d99c6ba2b2
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-13 23:46:44 +0300

    Update axon.js

--- scripts/axon.js
+++ scripts/axon.js
@@ -16,17 +16,17 @@ electroBasicBullet.shrinkX = 0.3;
 electroBasicBullet.spin = 3.5;
 electroBasicBullet.speed = 4;
 electroBasicBullet.damage = 43;
-electroSapBullet.shootEffect = Fx.railShoot;
-electroSapBullet.hitColor = electroSapBullet.frontColor = Color.valueOf("f1fc58");
-electroSapBullet.despawnEffect = Fx.railHit;
-electroSapBullet.lifetime = 90;
-electroSapBullet.knockback = 1;
-electroSapBullet.lightning = 5;//количество молний
-electroSapBullet.lightningLength = 5;//длина молнии
-electroSapBullet.lightningLengthRand = 50;//рандомное число от 0 до 50 будет прибавляться к длине молнии, то есть рандомизация длины
-electroSapBullet.lightningDamage = 20;//урон молнии
-electroSapBullet.lightningAngle = 43;//угол направления молний относительно угла пули
-electroSapBullet.lightningCone = 32;//рандомизация угла направления молний 
+electroBasicBullet.shootEffect = Fx.railShoot;
+electroBasicBullet.hitColor = electroSapBullet.frontColor = Color.valueOf("f1fc58");
+electroBasicBullet.despawnEffect = Fx.railHit;
+electroBasicBullet.lifetime = 90;
+electroBasicBullet.knockback = 1;
+electroBasicBullet.lightning = 5;//количество молний
+electroBasicBullet.lightningLength = 5;//длина молнии
+electroBasicBullet.lightningLengthRand = 50;//рандомное число от 0 до 50 будет прибавляться к длине молнии, то есть рандомизация длины
+electroBasicBullet.lightningDamage = 20;//урон молнии
+electroBasicBullet.lightningAngle = 43;//угол направления молний относительно угла пули
+electroBasicBullet.lightningCone = 32;//рандомизация угла направления молний 
 
 const axon = extendContent(ItemTurret, "axon", {});
 axon.health = 1260;
@@ -39,7 +39,7 @@ axon.description = "Powerful Electric shotgun.";
 axon.buildVisibility = BuildVisibility.shown;
 axon.requirements = ItemStack.with(Items.titanium, 200, Vars.content.getByName(ContentType.item,"braindustry-graphenite"), 480, Items.silicon, 350, Vars.content.getByName(ContentType.item,"braindustry-exotic-alloy"), 140);
 axon.category = Category.turret;
-axon.shootType = electroBasicBullet
+axon.shootType = electroBasicBullet;
 axon.ammo(
     Vars.content.getByName(ContentType.item,"braindustry-exotic-alloy"), electroBasicBullet, 
 );
