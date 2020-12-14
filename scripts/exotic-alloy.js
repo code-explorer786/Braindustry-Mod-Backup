db700896b5a03bb39777a47b8cc5613fdc1977be
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 22:55:38 +0300

    Update and rename items.js to exotic-alloy.js

---
+++ scripts/exotic-alloy.js
@@ -0,0 +1,11 @@
+//exometal
+const exoAlloy = extendContent(Item, "exotic-alloy", {});
+exoAlloy.localizedName = "Exo-Metal"
+exoAlloy.description = "Heavy organic metal with high durability and low weight."
+exoAlloy.cost = 3;
+exoAlloy.alwaysUnlocked = true;
+exoAlloy.explosiveness = 0.08;
+exoAlloy.radioactivity = 0;
+exoAlloy.flammability = 0.12;
+new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
+
