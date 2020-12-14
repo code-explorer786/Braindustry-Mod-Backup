5fc850c6a16f3efcd0975ea36691f132a0912180
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:56:21 +0300

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -7,15 +7,6 @@ exoAlloy.explosiveness = 0.08;
 exoAlloy.radioactivity = 0;
 exoticAlloy.color = "e2f723";
 exoAlloy.flammability = 0.12;
-//new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
-//graphenite
-const graphenIte = extendContent(Item, "graphenite", {});
-graphenIte.localizedName = "Graphenite"
-graphenIte.description = "Basic material for high-tec structures, has a low mass and normal durability.Produces in Graphenite forge."
-graphenIte.cost = 2;
-graphenIte.alwaysUnlocked = true;
-graphenIte.explosiveness = 0;
-graphenIte.radioactivity = 0;
-graphenIte.flammability = 0;
-new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")),graphenIte, ItemStack.with());
+new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
+
 
