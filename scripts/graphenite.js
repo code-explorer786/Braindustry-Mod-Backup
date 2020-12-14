eb84f97926905186d6aeb228bb42c7d556675b3b
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:44:55 +0300

    Delete graphenite.js

--- scripts/graphenite.js
+++
@@ -1,9 +0,0 @@
-const graphenIte = extendContent(Item, "graphenite", {});
-graphenIte.localizedName = "Graphenite"
-graphenIte.description = "Basic material for high-tec structures, has a low mass and normal durability.Produces in Graphenite forge."
-graphenIte.cost = 2;
-graphenIte.alwaysUnlocked = true;
-graphenIte.explosiveness = 0;
-graphenIte.radioactivity = 0;
-graphenIte.flammability = 0;
-new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")),graphenite, ItemStack.with());
