0afd188ffb2a32ad534ff91c65d34a7e55e59902
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:37:09 +0300

    Update exotic-alloy.js

--- scripts/exotic-alloy.js
+++ scripts/exotic-alloy.js
@@ -5,6 +5,7 @@ exoAlloy.cost = 3;
 exoAlloy.alwaysUnlocked = true;
 exoAlloy.explosiveness = 0.08;
 exoAlloy.radioactivity = 0;
+exoticAlloy.color = "e2f723";
 exoAlloy.flammability = 0.12;
 new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
 
