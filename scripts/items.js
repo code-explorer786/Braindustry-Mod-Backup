94520394a93c96cd0ab729b2b066fc288233b3e1
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:51:40 +0300

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -17,5 +17,5 @@ graphenIte.alwaysUnlocked = true;
 graphenIte.explosiveness = 0;
 graphenIte.radioactivity = 0;
 graphenIte.flammability = 0;
-new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")),graphenite, ItemStack.with());
+new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")),graphenIte, ItemStack.with());
 
