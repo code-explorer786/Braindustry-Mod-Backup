6bf6c2847f5ec2d7d0c1885365aa9f35ca01cde0
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:54:28 +0300

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -7,7 +7,7 @@ exoAlloy.explosiveness = 0.08;
 exoAlloy.radioactivity = 0;
 exoticAlloy.color = "e2f723";
 exoAlloy.flammability = 0.12;
-new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
+//new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
 //graphenite
 const graphenIte = extendContent(Item, "graphenite", {});
 graphenIte.localizedName = "Graphenite"
