0d4e18f5816c1255e99f601e45a275252a76d2bd
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:39:13 +0300

    Update graphenite.js

--- scripts/graphenite.js
+++ scripts/graphenite.js
@@ -1,9 +1,9 @@
-const graphenite = extendContent(Item, "graphenite", {});
-graphenite.localizedName = "Graphenite"
-graphenite.description = "Basic material for high-tec structures, has a low mass and normal durability.Produces in Graphenite forge."
-graphenite.cost = 2;
-graphenite.alwaysUnlocked = true;
-graphenite.explosiveness = 0;
-graphenite.radioactivity = 0;
-graphenite.flammability = 0;
+const graphenIte = extendContent(Item, "graphenite", {});
+graphenIte.localizedName = "Graphenite"
+graphenIte.description = "Basic material for high-tec structures, has a low mass and normal durability.Produces in Graphenite forge."
+graphenIte.cost = 2;
+graphenIte.alwaysUnlocked = true;
+graphenIte.explosiveness = 0;
+graphenIte.radioactivity = 0;
+graphenIte.flammability = 0;
 new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")),graphenite, ItemStack.with());
