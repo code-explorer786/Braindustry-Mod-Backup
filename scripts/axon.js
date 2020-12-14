7c93a202a056bcdcb2505a4c1e3097983ae10b2a
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-14 23:33:46 +0300

    Update axon.js

--- scripts/axon.js
+++ scripts/axon.js
@@ -9,29 +9,31 @@ this.super$update(b);
 }
 });
 electroBasicBullet.backColor = Color.valueOf("c2cc37");
-electroBasicBullet.width = 10;
-electroBasicBullet.height = 10;
+electroBasicBullet.width = 14;
+electroBasicBullet.height = 14;
 electroBasicBullet.shrinkY = 0.1;
-electroBasicBullet.shrinkX = 0.3;
+electroBasicBullet.shrinkX = 0.2;
 electroBasicBullet.spin = 3.5;
-electroBasicBullet.speed = 4;
+electroBasicBullet.speed = 2;
 electroBasicBullet.damage = 43;
 electroBasicBullet.shootEffect = Fx.railShoot;
 electroBasicBullet.hitColor = electroBasicBullet.frontColor = Color.valueOf("f1fc58");
 electroBasicBullet.despawnEffect = Fx.railHit;
 electroBasicBullet.lifetime = 90;
 electroBasicBullet.knockback = 1;
-electroBasicBullet.lightning = 5;//количество молний
-electroBasicBullet.lightningLength = 5;//длина молнии
-electroBasicBullet.lightningLengthRand = 50;//рандомное число от 0 до 50 будет прибавляться к длине молнии, то есть рандомизация длины
-electroBasicBullet.lightningDamage = 20;//урон молнии
-electroBasicBullet.lightningAngle = 43;//угол направления молний относительно угла пули
-electroBasicBullet.lightningCone = 32;//рандомизация угла направления молний 
+electroBasicBullet.lightning = 3;//количество молний
+electroBasicBullet.lightningLength = 1;//длина молнии
+electroBasicBullet.lightningLengthRand = 10;//рандомное число от 0 до 50 будет прибавляться к длине молнии, то есть рандомизация длины
+electroBasicBullet.lightningDamage = 21;//урон молнии
+electroBasicBullet.lightningAngle = 0;//угол направления молний относительно угла пули
+electroBasicBullet.lightningCone = 45;//рандомизация угла направления молний 
+electroBasicBullet.lightningColor = Color.valueOf("f1fc58");
 
 const axon = extendContent(ItemTurret, "axon", {});
 axon.health = 1260;
 axon.size = 3;
-axon.shots = 5;
+axon.rotateSpeed = 0.5;
+axon.shots = 3;
 axon.hasItems = true;
 axon.hasLiquids = true;
 axon.localizedName = "Axon";
