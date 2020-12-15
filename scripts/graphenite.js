e61cd6a09b10ec09955b5e7b9ff5452ff0927aec
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-15 10:38:42 +0300

    Update graphenite.js

--- scripts/graphenite.js
+++ scripts/graphenite.js
@@ -3,8 +3,7 @@ graphenite.localizedName = "Exo Metal"
 graphenite.description = "Progressed synthetic metal with low weight."
 graphenite.cost = 2;
 graphenite.alwaysUnlocked = true;
-graphenite.explosiveness = 0.08;
+graphenite.explosiveness = 0;
 graphenite.radioactivity = 0;
-graphenite.color = "e2f723";
-graphenite.flammability = 0.12;
+graphenite.flammability = 0;
 new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "graphite")), graphenite, ItemStack.with());
