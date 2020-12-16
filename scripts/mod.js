cca4b4f6daa1b61a89f1642309adfa609219a623
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-16 21:25:05 +0300

    Update mod.js

--- scripts/mod.js
+++ scripts/mod.js
@@ -1,7 +1,5 @@
 var mod = Vars.mods.locateMod("braindustry");
-var a = mod.meta;
-a.displayName = "[#f1d26e]Braindustry" ;
-a.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
-a.version = "1.4.2";
-a.minGameVersion = "121";
-a.author = "[#f1d26e]PixaxeOfPixie";
+var x = mod.meta;
+x.displayName = "[#f1d26e]Braindustry";
+x.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
+x.author = "[#f1d26e]PixaxeOfPixie";
