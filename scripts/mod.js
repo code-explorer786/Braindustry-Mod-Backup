0654bc25bd6b699d7d79a6fd581c32c493445ba8
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-16 21:10:06 +0300

    Create mod.js

---
+++ scripts/mod.js
@@ -0,0 +1,7 @@
+var mod = Vars.mods.locateMod("braindustry");
+var a = mod.meta;
+a.displayName = "[#f1d26e]Braindustry" ;
+a.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
+a.version = "1.4.2";
+a.minGameVersion = "121";
+a.author = "[#f1d26e]PixaxeOfPixie";
