09c7019c934cc98a7b7268420e227e90f4d55462
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-16 21:27:40 +0300

    Update mod.js

--- scripts/mod.js
+++ scripts/mod.js
@@ -1,5 +1,5 @@
 var mod = Vars.mods.locateMod("braindustry");
 var x = mod.meta;
-x.displayName = "[#f1d26e]Braindustry";
+x.displayName = "[#af6ecc]Braindustry";
 x.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
-x.author = "[#f1d26e]PixaxeOfPixie";
+x.author = "[#af6ecc]PixaxeOfPixie";
