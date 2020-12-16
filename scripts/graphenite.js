ca0daf610fa29bbf8864033bec7f4b22ab2da0a7
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-16 20:24:21 +0300

    Update graphenite.js

--- scripts/graphenite.js
+++ scripts/graphenite.js
@@ -3,6 +3,7 @@ graphenite.localizedName = "Graphenite"
 graphenite.description = "Progressed synthetic metal with low weight."
 graphenite.cost = 2;
 graphenite.alwaysUnlocked = true;
+graphenite.color = Color.valueOf("9868AB");
 graphenite.explosiveness = 0;
 graphenite.radioactivity = 0;
 graphenite.flammability = 0;
