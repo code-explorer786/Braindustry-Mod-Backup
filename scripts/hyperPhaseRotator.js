fd2fdb588861a6511cdd31a7bd7105c2de22a2e3
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-18 22:12:08 +0300

    Update hyperPhaseRotator.js

--- scripts/hyperPhaseRotator.js
+++ scripts/hyperPhaseRotator.js
@@ -1,2 +1,5 @@
 const hyperPhase = extendContent (GenericCrafter,"hyper-phase-weaver",{});
 hyperPhase.drawer = new DrawRotator();
+
+const exoSmelter = extendContent (GenericCrafter,"exotic-metal-smelter",{});
+exoSmelter.drawer = new DrawWeave();
