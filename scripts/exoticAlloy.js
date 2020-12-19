b76e35d1d97e8f5f9bec2a4730170a1abc646224
PixaxeOfPixie <63517945+pixaxeofpixie@users.noreply.github.com>
2020-12-19 16:46:46 +0300

    Update exoticAlloy.js

--- scripts/exoticAlloy.js
+++ scripts/exoticAlloy.js
@@ -1,5 +1,5 @@
 const exoAlloy = extendContent(Item, "exotic-alloy", {});
-exoAlloy.localizedName = "Exo Metal"
+//exoAlloy.localizedName = "Exo Metal"
 exoAlloy.description = "Heavy organic metal with high durability and low weight."
 exoAlloy.cost = 3;
 exoAlloy.alwaysUnlocked = true;
@@ -7,6 +7,6 @@ exoAlloy.explosiveness = 0.02;
 exoAlloy.color = Color.valueOf("e2f723");
 exoAlloy.radioactivity = 0;
 exoAlloy.flammability = 0.06;
-new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
+//new TechTree.TechNode(TechTree.all.find(boolf(t=>t.content.name == "surge-alloy")),exoAlloy, ItemStack.with());
 
 
