9627d8ec5f47e132f12038aa72d77176566c0d20
ThatOneBepis <47719370+ThatOneBepis@users.noreply.github.com>
2020-12-23 18:06:16 +0100

    fix

--- scripts/unitAbilities.js
+++ scripts/unitAbilities.js
@@ -1,44 +1,34 @@
 const armor = extendContent(UnitType, "armor", {});
 /*T1 unit*/
-armor.constructor = () => {
-const unit = extend(PayloadUnit, {
-})
-return unit
-}
+armor.constructor = function(){
+  return extend(PayloadUnit, {});
+};
 armor.abilities.add(new ForceFieldAbility(30, 5, 100, 1200));
 
 const shield = extendContent(UnitType, "shield", {});
 /*T2 unit*/
-shield.constructor = () => {
-const unit = extend(PayloadUnit, {
-})
-return unit
-}
+shield.constructor = function(){
+  return extend(PayloadUnit, {});
+};
 shield.abilities.add(new ForceFieldAbility(50, 6, 150, 1200));
 
 const chestplate = extendContent(UnitType, "chestplate", {});
 /*T3 unit*/
-chestplate.constructor = () => {
-const unit = extend(PayloadUnit, {
-})
-return unit
-}
+chestplate.constructor = function(){
+  return extend(PayloadUnit, {});
+};
 chestplate.abilities.add(new ForceFieldAbility(70, 7, 220, 1200));
 
 const chainmail = extendContent(UnitType, "chainmail", {});
 /*T4 unit with heal field*/
-chainmail.constructor = () => {
-const unit = extend(PayloadUnit {
-})
-return unit
-}
+chainmail.constructor = function(){
+  return extend(PayloadUnit, {});
+};
 chainmail.abilities.add(new ForceFieldAbility(120, 7, 220, 1200), new RepairFieldAbility(90, 60, 140));
 
 const broadsword = extendContent(UnitType, "broadsword", {});
 /*T5 unit with heal field*/
-broadsword.constructor = () => {
-const unit = extend(PayloadUnit, {
-})
-return unit
-}
+broadsword.constructor = function(){
+  return extend(PayloadUnit, {});
+};
 broadsword.abilities.add(new ForceFieldAbility(160, 8, 220, 1200), new RepairFieldAbility(120, 80, 210));
