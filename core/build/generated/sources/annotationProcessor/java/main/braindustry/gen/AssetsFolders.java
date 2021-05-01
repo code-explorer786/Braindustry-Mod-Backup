e843886bb314d58c30ebfbf52d451fcd3097a492
Antarex <63517945+pixaxeofpixie@users.noreply.github.com>
2021-05-01 18:19:23 +0300

    i hate this day

--- core/build/generated/sources/annotationProcessor/java/main/braindustry/gen/AssetsFolders.java
+++
@@ -1,9 +0,0 @@
-package braindustry.gen;
-
-import arc.struct.Seq;
-
-public class AssetsFolders {
-  public static Seq<String> getFolders() {
-    return Seq.with(".DS_Store","bundles","icon.png","maps","music","schematics","shaders","sounds","sprites","sprites-override");
-  }
-}
