039b938bba5bb9da8cc15b33d4a72d9f4a1551f7
Zelaux <ura20060605@gmail.com>
2021-05-03 23:27:47 +0300

    Displaying effects over rotors in MultiRotorDrill

---
+++ core/src/braindustry/type/Rotor.java
@@ -0,0 +1,21 @@
+package braindustry.type;
+
+public class Rotor {
+    public float x;
+    public float y;
+    public float size=-1;
+
+    public Rotor(float x, float y, float size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+    }
+
+    public Rotor(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+    public interface DrillRotorCons{
+        void get(float x,float y,float rotorSize);
+    }
+}
