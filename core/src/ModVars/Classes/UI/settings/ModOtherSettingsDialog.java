ec6b3b21016a0a52ffde6b40ab508e32b5e86ac2
Zelaux <ura20060605@gmail.com>
2021-05-17 20:09:22 +0300

    -Added non-existent translations -Move buttons and others in Braindustry settings.

--- core/src/ModVars/Classes/UI/settings/ModOtherSettingsDialog.java
+++ core/src/ModVars/Classes/UI/settings/ModOtherSettingsDialog.java
@@ -22,25 +22,26 @@ public class ModOtherSettingsDialog extends Dialog {
 
         cont.table(t->{
             t.left().defaults();
+            Cell<Label> label = t.label(() -> Strings.format("@: @/@", Core.bundle.get("background.screenshot.scl"), (int) modVars.settings.getFloat("background.screenshot.scl"), 5)).left();
             t.slider(1f,5f,1f,modVars.settings.getFloat("background.screenshot.scl"),f->{
                 modVars.settings.setFloat("background.screenshot.scl",f);
             }).left().width(180.0F);
-            Cell<Label> label = t.label(() -> Strings.format("@: @/@", Core.bundle.get("background.screenshot.scl"), (int) modVars.settings.getFloat("background.screenshot.scl"), 5)).left();
             label.minWidth(label.get().getPrefWidth() / Scl.scl(1.0F) + 50.0F);
             t.row();
-        }).padTop(3.0F).row();
+        }).padTop(3.0F).left().row();
         cont.table((t)->{
             t.check("@cheat", modVars.settings.cheating(),(b)->{
                 modVars.settings.cheating(b);
-            }).row();
+            }).left().row();
             t.check("@debug",modVars.settings.debug(),(b)->{
                 modVars.settings.debug(b);
-            }).row();
+            }).left().row();
+
             t.slider(0,360,0.1f,modVars.settings.getFloat("angle"),(b)->{
                 modVars.settings. setFloat("angle",b);
-            }).row();
+            }).left().row();
 
-        }).row();
+        }).left().row();
 
         closeOnBack();
         cont.button("@back", Icon.leftOpen, () -> {
